fba0f3dfd9538f0f466f1b8da2258dd081308fa3
Franklyn R <franklyn1998@live.com>
2022-10-22 11:53:39 -0300

    Componente Início (Home)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,9 @@ import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
 
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
+import { HomeComponent } from "./views/home/home.component";
 
 @NgModule({
     declarations: [
@@ -18,6 +20,7 @@ import { MatListModule } from "@angular/material/list";
         HeaderComponent,
         FooterComponent,
         NavComponent,
+        HomeComponent,
     ],
     imports: [
         BrowserModule,
@@ -25,6 +28,7 @@ import { MatListModule } from "@angular/material/list";
         MatToolbarModule,
         MatSidenavModule,
         MatListModule,
+        MatCardModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
