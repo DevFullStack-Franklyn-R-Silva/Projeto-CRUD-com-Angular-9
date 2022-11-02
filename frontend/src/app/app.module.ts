168df8c6c28da1a563b718a114b19b8b52c88bac
Franklyn R <franklyn1998@live.com>
2022-11-02 10:28:26 -0300

    Diretivas na Prática #01

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/template/header/header.component";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
 
-import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { RedDirective } from "./directives/red.directive";
 import { HomeComponent } from "./views/home/home.component";
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
 
 @NgModule({
     declarations: [
@@ -24,6 +25,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
         NavComponent,
         HomeComponent,
         ProductCrudComponent,
+        RedDirective,
     ],
     imports: [
         BrowserModule,
