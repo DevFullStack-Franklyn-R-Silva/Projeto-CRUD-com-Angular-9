d52fd2f98ccfad4938d64b608146e6cd4c8f4369
Franklyn R <franklyn1998@live.com>
2022-11-02 12:05:16 -0300

    Formulário Criar Produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
@@ -43,6 +46,9 @@ import { ProductCrudComponent } from "./views/product-crud/product-crud.componen
         MatButtonModule,
         MatSnackBarModule,
         HttpClientModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
