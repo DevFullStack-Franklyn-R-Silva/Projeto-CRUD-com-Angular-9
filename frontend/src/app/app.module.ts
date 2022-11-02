a379ad7db3b231cd83a0bf00e18fd1040447c303
Franklyn R <franklyn1998@live.com>
2022-11-02 12:16:15 -0300

    Componente Obter Produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ForDirective } from "./directives/for.directive";
 import { RedDirective } from "./directives/red.directive";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
     declarations: [
@@ -34,6 +35,7 @@ import { ProductCrudComponent } from "./views/product-crud/product-crud.componen
         RedDirective,
         ForDirective,
         ProductCreateComponent,
+        ProductReadComponent,
     ],
     imports: [
         BrowserModule,
---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,21 @@
+import { ProductService } from "./../product.service";
+import { Product } from "./../product.model";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-product-read",
+    templateUrl: "./product-read.component.html",
+    styleUrls: ["./product-read.component.css"],
+})
+export class ProductReadComponent implements OnInit {
+    products: Product[] = [];
+
+    constructor(private ProductService: ProductService) {}
+
+    ngOnInit(): void {
+        this.ProductService.read().subscribe((products) => {
+            this.products = products;
+            console.log(products);
+        });
+    }
+}
