0ab9a30cd3cc96ecec5194ca25b71d6eb39a5dd8
Franklyn R <franklyn1998@live.com>
2022-11-02 11:29:46 -0300

    Produto Service

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from "./../product.service";
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-product-create",
@@ -6,7 +8,17 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
-    constructor() {}
+    constructor(
+        private productService: ProductService,
+        private router: Router
+    ) {}
 
     ngOnInit(): void {}
+    createProduct(): void {
+        this.productService.showMessage("Produto criado!");
+    }
+
+    cancel(): void {
+        this.router.navigate(["/products"])
+    }
 }
