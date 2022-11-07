c0a1b8c40fdfb0f77cbee3cbc35b33ef8a9caf61
Franklyn R <franklyn1998@live.com>
2022-11-07 20:19:12 -0300

    Componente Atualizar Produto #01

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { ForDirective } from "./directives/for.directive";
 import { RedDirective } from "./directives/red.directive";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
 
 registerLocaleData(localePt);
 
@@ -45,6 +46,7 @@ registerLocaleData(localePt);
         ProductCreateComponent,
         ProductReadComponent,
         ProductRead2Component,
+        ProductUpdateComponent,
     ],
     imports: [
         BrowserModule,
