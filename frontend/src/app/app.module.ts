929f239c34534a30e58e6db8ba4f1c5fb9a83219
Franklyn R <franklyn1998@live.com>
2022-11-07 19:55:08 -0300

    Tabela Produtos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,18 @@
+import { registerLocaleData } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
+import localePt from "@angular/common/locales/pt";
+import { LOCALE_ID, NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -15,6 +20,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { ProductReadComponent } from "./components/product/product-read/product-read.component";
+import { ProductRead2Component } from "./components/product/product-read2/product-read2.component";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { HeaderComponent } from "./components/template/header/header.component";
 import { NavComponent } from "./components/template/nav/nav.component";
@@ -22,11 +29,8 @@ import { ForDirective } from "./directives/for.directive";
 import { RedDirective } from "./directives/red.directive";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
+
+registerLocaleData(localePt);
 
 @NgModule({
     declarations: [
@@ -60,7 +64,12 @@ import { MatSortModule } from '@angular/material/sort';
         MatPaginatorModule,
         MatSortModule,
     ],
-    providers: [],
+    providers: [
+        {
+            provide: LOCALE_ID,
+            useValue: "pt-BR",
+        },
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
