4297353b5f502b673e1a3171fbba818098f1d160
Franklyn R <franklyn1998@live.com>
2022-11-07 19:40:59 -0300

    Entendendo Componente Gerado

--- frontend/src/app/components/product/product-read2/product-read2.component.ts
+++ frontend/src/app/components/product/product-read2/product-read2.component.ts
@@ -1,30 +1,31 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { ProductRead2DataSource, ProductRead2Item } from './product-read2-datasource';
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTable } from "@angular/material/table";
+import { Product } from "./../product.model";
+import { ProductRead2DataSource } from "./product-read2-datasource";
 
 @Component({
-  selector: 'app-product-read2',
-  templateUrl: './product-read2.component.html',
-  styleUrls: ['./product-read2.component.css']
+    selector: "app-product-read2",
+    templateUrl: "./product-read2.component.html",
+    styleUrls: ["./product-read2.component.css"],
 })
 export class ProductRead2Component implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<ProductRead2Item>;
-  dataSource: ProductRead2DataSource;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+    @ViewChild(MatTable) table!: MatTable<Product>;
+    dataSource: ProductRead2DataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+    /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+    displayedColumns = ["id", "name", "price"];
 
-  constructor() {
-    this.dataSource = new ProductRead2DataSource();
-  }
+    constructor() {
+        this.dataSource = new ProductRead2DataSource();
+    }
 
-  ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
-  }
+    ngAfterViewInit(): void {
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.table.dataSource = this.dataSource;
+    }
 }
